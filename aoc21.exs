d6062c0caaa395ad0fcebcba436aace502bc9eb6
Michal Blazejczyk <michal.blazejczyk@gmail.com>
2022-10-13 21:42:07 -0400

    Solution to day 2, part 2

--- aoc21.exs
+++ aoc21.exs
@@ -3,4 +3,5 @@
 # IO.puts("Part 2 result: #{Day01.part2()}")
 
 IO.puts("::: Day 2 :::")
-IO.puts("Part 2 result: #{Day02.part1()}")
+IO.puts("Part 1 result: #{Day02.part1()}")
+IO.puts("Part 2 result: #{Day02.part2()}")
--- lib/day02.ex
+++ lib/day02.ex
@@ -2,6 +2,7 @@ defmodule Day02 do
   @spec get_data :: [{String.t(), integer()}]
   def get_data() do
     Tools.read_file("day02.txt")
+    # ["forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2"]
     |> Enum.map(&String.split/1)
     |> Enum.map(fn [instruction, value_str] -> {instruction, String.to_integer(value_str)} end)
   end
@@ -17,4 +18,23 @@ defmodule Day02 do
   defp process_row_for_part1({"forward", value}, {depth, distance}), do: {depth, distance + value}
   defp process_row_for_part1({"up", value},      {depth, distance}), do: {depth - value, distance}
   defp process_row_for_part1({"down", value},    {depth, distance}), do: {depth + value, distance}
+
+  @spec part2 :: number()
+  def part2() do
+    get_data()
+    |> Enum.reduce({0, 0, 0}, &process_row_for_part2/2)
+    |> Kernel.then(fn {depth, distance, _aim} -> depth * distance end)
+  end
+
+  @spec process_row_for_part2({String.t(), integer()}, {integer(), integer(), integer()})
+    :: {integer(), integer(), integer()}
+  defp process_row_for_part2({"forward", value}, {depth, distance, aim}) do
+    {depth + aim * value, distance + value, aim}
+  end
+  defp process_row_for_part2({"up", value}, {depth, distance, aim}) do
+    {depth, distance, aim - value}
+  end
+  defp process_row_for_part2({"down", value}, {depth, distance, aim}) do
+    {depth, distance, aim + value}
+  end
 end
