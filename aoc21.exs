ca3d1444cee0c89bfea8a9a72f36e6bc8743e541
Michal Blazejczyk <michal.blazejczyk@gmail.com>
2022-10-20 21:47:18 -0400

    Renamed a variable

--- aoc21.exs
+++ aoc21.exs
@@ -4,16 +4,16 @@
 # mix run aoc21.exs 2
 
 [day | rest] = System.argv()
-test_data = length(rest) >= 1 && hd(rest) == "test"
+test_data? = length(rest) >= 1 && hd(rest) == "test"
 module_name = "Elixir.Day" <> String.pad_leading(day, 2, "0")
 module = String.to_existing_atom(module_name)
 
 IO.puts("::: Day #{day} :::")
-if test_data do
+if test_data? do
   IO.puts("... test data ...")
 else
   IO.puts("... real data ...")
 end
 
-IO.puts("Part 1 result: #{module.part1(test_data)}")
-IO.puts("Part 2 result: #{module.part2(test_data)}")
+IO.puts("Part 1 result: #{module.part1(test_data?)}")
+IO.puts("Part 2 result: #{module.part2(test_data?)}")
