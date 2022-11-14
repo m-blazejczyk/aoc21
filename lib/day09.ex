35fb5ab13d263bad66cc04751d1a95915c5cf942
Michal Blazejczyk <michal.blazejczyk@gmail.com>
2022-11-13 21:45:26 -0500

    Solution for day 9, part 2

--- lib/day09.ex
+++ lib/day09.ex
@@ -31,7 +31,7 @@ defmodule Day09 do
 
     find_low_points_partial = Tools.partial_2args(&find_low_points/3, [coords_map])
 
-    low_points = get_data(test_data)
+    low_points = coords_map
     |> Enum.reduce([], find_low_points_partial)
 
     (low_points |> Enum.sum()) + length(low_points)
@@ -59,7 +59,53 @@ defmodule Day09 do
 
   @spec part2(boolean()) :: number()
   def part2(test_data) do
-    get_data(test_data)
-    0
+    coords_map = get_data(test_data)
+
+    find_basins_partial = Tools.partial_2args(&find_basins/3, [coords_map])
+
+    {_, basin_sizes} = coords_map
+    |> Enum.reduce({MapSet.new(), []}, find_basins_partial)
+
+    basin_count = length(basin_sizes)
+    basin_sizes
+    |> Enum.sort()
+    |> Enum.drop(basin_count - 3)
+    |> Enum.product()
+  end
+
+  @spec find_basins(map(), {{integer(), integer()}, integer()}, {MapSet.t(), [integer()]})
+    :: {MapSet.t(), [integer()]}
+  defp find_basins(_coords_map, {coord, depth}, {visited, basin_lengths})
+    when depth == 9 do
+    # If we run into a point at depth 9 - we just add them to 'visited' and move on
+    {visited |> MapSet.put(coord), basin_lengths}
+  end
+  defp find_basins(coords_map, {coord, _depth}, {visited, basin_lengths} = acc) do
+    if visited |> MapSet.member?(coord) do
+      # If this coordinate has already been visited, we skip it
+      acc
+    else
+      # We've identified the first location of an unvisited basin.
+      # We will now recursively explore this basin from this location.
+      prev_size = visited |> MapSet.size()
+      new_visited = expand_basin(coords_map, coord, visited)
+      {new_visited, [(MapSet.size(new_visited) - prev_size) | basin_lengths]}
+    end
+  end
+
+  @spec expand_basin(map(), {integer(), integer()}, MapSet.t()) :: MapSet.t()
+  defp expand_basin(coords_map, {row, col} = coord, visited) do
+    if coords_map |> Map.get(coord, 9) != 9
+      &&
+      not(visited |> MapSet.member?(coord))
+      do
+      new_visited = visited |> MapSet.put(coord)
+      expand_basin_partial = Tools.partial_2args(&expand_basin/3, [coords_map])
+
+      [{row - 1, col}, {row + 1, col}, {row, col - 1}, {row, col + 1}]
+      |> Enum.reduce(new_visited, expand_basin_partial)
+    else
+      visited
+    end
   end
 end
