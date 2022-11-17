8eef2c9447699bb5e03f5fdf90015f0a80818da0
Michal Blazejczyk <michal.blazejczyk@gmail.com>
2022-11-16 22:22:58 -0500

    Improved pattern matching

--- lib/day10.ex
+++ lib/day10.ex
@@ -37,21 +37,21 @@ defmodule Day10 do
   # we're breaking out of the loop
   defp handle_char(_, result) when is_integer(result), do: result
   # We found an opening bracket; add it on top of the stack (the accumulator)
-  defp handle_char(c, stack) when c == ?(, do: [c | stack]
-  defp handle_char(c, stack) when c == ?[, do: [c | stack]
-  defp handle_char(c, stack) when c == ?<, do: [c | stack]
-  defp handle_char(c, stack) when c == ?{, do: [c | stack]
+  defp handle_char(?( = c, stack), do: [c | stack]
+  defp handle_char(?[ = c, stack), do: [c | stack]
+  defp handle_char(?< = c, stack), do: [c | stack]
+  defp handle_char(?{ = c, stack), do: [c | stack]
   # We found a closing bracket that corresponds to the opening character
   # on top of the stack: "pop" it
-  defp handle_char(c, [prev | rest]) when c == ?) and prev == ?(, do: rest
-  defp handle_char(c, [prev | rest]) when c == ?] and prev == ?[, do: rest
-  defp handle_char(c, [prev | rest]) when c == ?> and prev == ?<, do: rest
-  defp handle_char(c, [prev | rest]) when c == ?} and prev == ?{, do: rest
+  defp handle_char(?), [?( | rest]), do: rest
+  defp handle_char(?], [?[ | rest]), do: rest
+  defp handle_char(?>, [?< | rest]), do: rest
+  defp handle_char(?}, [?{ | rest]), do: rest
   # Invalid closing bracket
-  defp handle_char(c, _) when c == ?), do: 3
-  defp handle_char(c, _) when c == ?], do: 57
-  defp handle_char(c, _) when c == ?>, do: 25137
-  defp handle_char(c, _) when c == ?}, do: 1197
+  defp handle_char(?), _), do: 3
+  defp handle_char(?], _), do: 57
+  defp handle_char(?>, _), do: 25137
+  defp handle_char(?}, _), do: 1197
 
   @spec part2(boolean()) :: number()
   def part2(test_data) do
