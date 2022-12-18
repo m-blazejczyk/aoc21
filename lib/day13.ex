da7ebebb6572b54b42c3d35148fb8d3b978f2e7e
Michal Blazejczyk <michal.blazejczyk@gmail.com>
2022-12-17 23:50:08 -0500

    Day 13, part 2: added complete folding (unverified)

--- lib/day13.ex
+++ lib/day13.ex
@@ -67,17 +67,37 @@ defmodule Day13 do
 
   @spec fold({:fold_x | :fold_y, integer()}, MapSet.t()) :: MapSet.t()
   defp fold({:fold_y, fold_coord}, dots) do
-    dots
+    # This folds the dots along Y coordinate of 'fold_coord'
+    # After the transformation, the Y coordinates decrease towards the folding line
+    folded = dots
     |> Enum.reduce(MapSet.new(), fn {x, y}, set -> set |> MapSet.put({x, abs(fold_coord - y)}) end)
+
+    # What is the largest Y coordinate after the fold?
+    {_x, max_coord} = folded
+    |> Enum.max_by(fn {_x, y} -> y end)
+
+    # Now we scale the coordinates so that they start from zero again
+    folded
+    |> Enum.reduce(MapSet.new(), fn {x, y}, set -> set |> MapSet.put({x, max_coord - y}) end)
   end
   defp fold({:fold_x, fold_coord}, dots) do
-    dots
+    folded = dots
     |> Enum.reduce(MapSet.new(), fn {x, y}, set -> set |> MapSet.put({abs(fold_coord - x), y}) end)
+
+    {max_coord, _y} = folded
+    |> Enum.max_by(fn {x, _y} -> x end)
+
+    folded
+    |> Enum.reduce(MapSet.new(), fn {x, y}, set -> set |> MapSet.put({max_coord - x, y}) end)
   end
 
   @spec part2(boolean()) :: number()
   def part2(test_data) do
-    get_data(test_data)
-    0
+    {dots, folds} = get_data(test_data)
+
+    folds
+    |> Enum.reduce(dots, &fold/2)
+    |> IO.inspect()
+    |> MapSet.size()
   end
 end
